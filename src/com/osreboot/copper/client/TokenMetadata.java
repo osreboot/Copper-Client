5839480e586e7d2477b0a1a8044f1183306b1c84
osreboot <calvin.16.weaver@gmail.com>
2022-01-17 03:46:25 -0800

    Optional procedural voids between asteroids

--- src/com/osreboot/copper/client/TokenMetadata.java
+++ src/com/osreboot/copper/client/TokenMetadata.java
@@ -5,12 +5,23 @@ import java.io.Serializable;
 public final class TokenMetadata implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
-	public static final TokenMetadata DEV = new TokenMetadata("dev");
+	public static final TokenMetadata DEV = new TokenMetadata("dev",
+			false, "dev", 1f);
 	
 	public String seedTerrain;
 	
-	public TokenMetadata(String seedTerrainArg){
+	public boolean hasVoids;
+	public String seedVoids;
+	public float scaleVoids;
+	// TODO offsetVoids
+	
+	public TokenMetadata(String seedTerrainArg,
+			boolean hasVoidsArg, String seedVoidsArg, float scaleVoidsArg){
 		seedTerrain = seedTerrainArg;
+		
+		hasVoids = hasVoidsArg;
+		seedVoids = seedVoidsArg;
+		scaleVoids = scaleVoidsArg;
 	}
 	
 }
--- src/com/osreboot/copper/client/forge/GeneratorSurfaceProbability.java
+++ src/com/osreboot/copper/client/forge/GeneratorSurfaceProbability.java
@@ -16,7 +16,7 @@ public final class GeneratorSurfaceProbability {
 
 	public static final float
 	SEED_RADIUS_BUFFER = 2f,
-	SEED_RADIUS_BUFFER_RANDOM = 20f,
+	SEED_RADIUS_BUFFER_RANDOM = 10f,
 	SEED_RADIUS_MIN = 3f,
 	SEED_RADIUS_MAX = 50f,
 	SPAWN_RADIUS = 2f,
@@ -26,8 +26,14 @@ public final class GeneratorSurfaceProbability {
 	SEED_ATTEMPTS = 16;
 
 	public static Mask<Float> run(TokenMetadata metadata){
-		Random random = new Random(metadata.seedTerrain.hashCode());
-		
+		// (Voids) Initialize noise
+		Random randomVoids = new Random(metadata.seedVoids.hashCode());
+		Noise2DPerlin noiseVoids = new Noise2DPerlin(randomVoids, 5,
+				new HvlCoord(EWorld.DIAMETER * metadata.scaleVoids / -2f, EWorld.DIAMETER * metadata.scaleVoids / -2f),
+				new HvlCoord(EWorld.DIAMETER * metadata.scaleVoids / 2f, EWorld.DIAMETER * metadata.scaleVoids / 2f));
+
+		Random randomTerrain = new Random(metadata.seedTerrain.hashCode());
+
 		Mask<Float> maskCellProbability = new Mask<>(0f);
 
 		// Select seeds based on Poisson disc sampling
@@ -35,23 +41,24 @@ public final class GeneratorSurfaceProbability {
 		ArrayList<SeedAsteroid> seeds = new ArrayList<>();
 
 		SeedAsteroid seedInitial = new SeedAsteroid(new HvlCoord(
-				random.nextFloat() * EWorld.DIAMETER - EWorld.DIAMETER / 2f,
-				random.nextFloat() * EWorld.DIAMETER - EWorld.DIAMETER / 2f), getRandomRadius(random));
+				randomTerrain.nextFloat() * EWorld.DIAMETER - EWorld.DIAMETER / 2f,
+				randomTerrain.nextFloat() * EWorld.DIAMETER - EWorld.DIAMETER / 2f), getRandomRadius(randomTerrain));
 		seedsActive.add(seedInitial);
 		seeds.add(seedInitial);
 
 		while(seedsActive.size() > 0){
-			SeedAsteroid seedCurrent = seedsActive.get(random.nextInt(seedsActive.size()));
+			SeedAsteroid seedCurrent = seedsActive.get(randomTerrain.nextInt(seedsActive.size()));
 			for(int i = 0; i < SEED_ATTEMPTS; i++){
-				float testAngle = random.nextFloat() * 2f * (float)Math.PI;
-				float testRadius = getRandomRadius(random);
-				float testDistance = getRandomBuffer(random) + seedCurrent.radius + testRadius;
+				float testAngle = randomTerrain.nextFloat() * 2f * (float)Math.PI;
+				float testRadius = getRandomRadius(randomTerrain);
+				float testDistance = getRandomBuffer(randomTerrain) + seedCurrent.radius + testRadius;
 				HvlCoord testLocation = new HvlCoord((float)Math.cos(testAngle) * testDistance, (float)Math.sin(testAngle) * testDistance).add(seedCurrent.location);
 				SeedAsteroid seedTest = new SeedAsteroid(testLocation, testRadius);
 				if(Math.abs(testLocation.x) + testRadius < EWorld.DIAMETER / 2f && Math.abs(testLocation.y) + testRadius < EWorld.DIAMETER / 2f){
 					boolean valid = true;
+
 					for(SeedAsteroid seed : seeds){
-						if(HvlMath.distance(testLocation, seed.location) < testRadius + seed.radius + getRandomBuffer(random) ||
+						if(HvlMath.distance(testLocation, seed.location) < testRadius + seed.radius + getRandomBuffer(randomTerrain) ||
 								HvlMath.distance(testLocation, new HvlCoord()) < testRadius + SPAWN_RADIUS){
 							valid = false;
 							break;
@@ -65,7 +72,13 @@ public final class GeneratorSurfaceProbability {
 			}
 			seedsActive.remove(seedCurrent);
 		}
+		
+		// (Voids) Remove asteroids based on noise mask
+		seeds.removeIf(seed -> {
+			return metadata.hasVoids && (randomTerrain.nextFloat() - 0.5f) * 2f/* - (seed.radius / 30f)*/ < noiseVoids.map(seed.location) * 4f;
+		});
 
+		// Assign output mask probability values
 		ForgeUtil.forWorld((x, y) -> {
 			HvlCoord tLocation = WorldUtil.toEntitySpace(new HvlCoord(x, y));
 			for(SeedAsteroid seed : seeds){
@@ -82,7 +95,7 @@ public final class GeneratorSurfaceProbability {
 				Math.min(seed.radius * 1.5f, seed.radius + FIELD_SIZE_LIMIT),
 				1f, 0f), 0f, 1f);
 	}
-	
+
 	private static float getRandomRadius(Random random){
 		return (float)Math.pow(0.01f, random.nextFloat()) * (SEED_RADIUS_MAX - SEED_RADIUS_MIN) + SEED_RADIUS_MIN;
 	}
